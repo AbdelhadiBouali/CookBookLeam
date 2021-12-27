44c68db5bd2b6d2fc7e7a1aa23a1a83994db0c8a
unknown <ha_bouali@esi.dz>
2021-12-27 02:08:54 +0100

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CookBookApp());
+}
+
+class CookBookApp extends StatelessWidget {
+  const CookBookApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
